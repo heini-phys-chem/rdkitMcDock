217e486fc27627f305b2011f59fd01b9be2fe469
Heini <heini.phys.chem@gmail.com>
2023-03-16 10:00:15 -0400

    Add files via upload

---
+++ mcDock_rdkit.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+import sys, os
+import random
+import warnings
+
+import numpy as np
+import openbabel
+from openbabel import openbabel as ob
+
+from rdkit import Chem
+
+from utils_rdkit import *
+
+# Suppress RuntimeWarning from first Metropolis-Hastings MC criterian, accept
+warnings.filterwarnings("ignore")
+os.system("rm -f out.xyz min.xyz directory/*.xyz")
+
+def main():
+    print("\nUsing OpenBabel version: {}\n------------------------------\n".format(openbabel.__version__))
+    # Read cmd line arguments
+    f_target, f_ligand, force_field, trajectories, steps, temperature = get_options(sys.argv[1:])
+
+    # Read in molecules
+    # Host = target
+    target = readfile(f_target)
+    # Guest = ligand
+    ligand = readfile(f_ligand)
+    print(" [+] Read molecules")
+
+    # center ligand molecule
+    com = get_com(ligand)
+    print(" [+] get center of mass (ligand)")
+
+    ligand = move_molecule(ligand, -com)
+    print(" [+] move molecule to center (ligand)")
+
+    # center target molecule
+    com = get_com(target)
+    print(" [+] get center of mass (target)")
+
+    target = move_molecule(target, -com)
+    print(" [+] move molecule to center (target) \n")
+
+    numTot = target.GetNumAtoms() + ligand.GetNumAtoms()
+
+    eb = calc_energy_xTB(ligand)
+    ea = calc_energy_xTB(target)
+
+    print()
+    print("Energy Host before opt: {}".format(ea))
+    print("Energy Guest before opt: {}".format(eb))
+    print()
+
+    ea, target = minimize_molecule_xTB(target)
+    eb, ligand = minimize_molecule_xTB(ligand)
+
+    print()
+    print("Energy Host after opt: {}".format(ea))
+    print("Energy Guest after opt: {}".format(eb))
+    print()
+
+    #writefile_xyz(ligand, "ligand_xtbopt_from_mcDock.xyz")
+    #writefile(target, "target_xtbopt_from_mcDock.sdf")
+
+    numTot = target.GetNumAtoms() + ligand.GetNumAtoms()
+    e_low  = np.Inf
+    eb_min = np.Inf
+
+
+    c = 1
+    numConfs = 1
+    # Perfomr conformer search
+#    print(" Perfomring rotor search for ligand molecule      file: {}".format(f_ligand))
+#    ligand = set_conformations(ligand, force_field)
+#
+#    numConfs = ligand.NumConformers()
+#
+#    for c in range(1, numConfs+1):
+#        ligand.SetConformer(c)
+#
+#        eb, ligand = minimize_molecule(ligand, force_field)
+#
+#        if eb < eb_min:
+#            eb_min = eb
+#
+#        print(" Rotamere {}     E = {:.4f} kcal/mol".format(str(c), eb))
+#
+#    print(" Lowers energy conformation E = {:.4f} kcal/mol".format(eb_min))
+#    print(" Running {} trajectories for {} steps".format(trajectories, steps))
+#    print(" MC temperature (tau) = {}".format(temperature))
+
+
+    # Start the MC simulation
+    print("\n Conformation:         Trajectory:         Acceptance rate:               Final Ebind:")
+    print(" -------------------------------------------------------------------------------------")
+
+    # Loop over conformers
+#    for c in range(1, numConfs+1):
+#    ligand.SetConformer(c)
+
+#    eb, ligand = minimize_molecule_xTB(ligand)
+
+    # For every conformer loop over number of trajectories
+    for n in range(int(trajectories)):
+        # translate molecule
+        #direction = random_vector()
+        #com = get_com(ligand)
+        #temp = direction * 4.0 - com
+        #move_molecule(ligand, temp)
+
+        # rotate molecule (Guest)
+        rot = random_vector()
+        theta = random_angle()# * np.pi
+        rotate_molecule(ligand, rot, theta)
+
+        # roll back to rejected MC moves
+        # add both molecule to the same "system"
+        mol_ligand = plus_equal_mols(target, ligand, numTot)
+        mol_old    = mol_ligand
+        #mol_old.SetCoordinates(mol_ligand.GetCoordinates())
+
+#        ff = AllChem.MMFFGetMoleculeForceField(mol_ligand)
+#        e = AllChem.UFFGetMoleculeForceField(mol_ligand).CalcEnergy()
+        e = calc_energy_xTB(mol_ligand)
+
+        energy_old = e
+        delta_e = 0.0
+        accept = 0
+
+        startid = mol_ligand.GetNumAtoms() - ligand.GetNumAtoms()
+        endid   = mol_ligand.GetNumAtoms()
+
+        # MC simulation for every trajectory
+        for step in range(int(steps)):
+            move = random_vector()
+            move *= random_length()
+            #move_molecule(mol_ligand, move, startid=startid, endid=endid)
+
+            rot   = random_vector()
+            theta = random_angle()
+            rotate_molecule(mol_ligand, rot, theta, startid=startid, endid=endid)
+
+
+            #e = AllChem.UFFGetMoleculeForceField(mol_ligand).CalcEnergy()
+            e = calc_energy_xTB(mol_ligand)
+
+            delta_e = e - energy_old
+
+            # Metropolis-Hastings MC criterian, accept ...
+            threash_hold = uniform1()
+            if np.exp(-delta_e / float(temperature)) >= threash_hold:# and range_test:
+                mol_old = mol_ligand
+                energy_old = e
+                accept += 1
+            else:
+                mol_ligand = mol_old
+                e = energy_old
+
+
+        ec, mol_ligand = minimize_molecule_xTB(mol_ligand)
+
+        e_bind = ec - (ea + eb)
+
+        acceptance_ratio = accept * 100.0 / (float(steps) + 1)
+        acceptance_ratio = round(acceptance_ratio, 2)
+        print(" {} / {}\t\t\t{} / {}\t\t\t{}\t%\t\t{:4.2f}\tkcal/mol".format(c, numConfs, str(n+1).zfill(2), trajectories, str(acceptance_ratio).zfill(2), e_bind), end = '')
+
+
+        writefile_xyz(mol_ligand, "directory/out_{}_{}_{}.xyz".format(c, n, 1))
+
+
+        if (e_bind < e_low):
+            print("     <----- New Lowest")
+            e_low = e_bind
+            writefile_xyz(mol_ligand, "min.xyz")
+        else:
+            print()
+
+    os.system("cat directory/out_*.xyz >> out.xyz")
+    #os.system("rm -rf out_*.xyz")
+
+    print("\n\n [+] Optimization done")
+
+if __name__ == '__main__':
+    main()
---
+++ utils_rdkit.py
@@ -0,0 +1,343 @@
+import sys, os, getopt
+
+import numpy as np
+from openbabel import openbabel as ob
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Geometry import Point3D
+
+from ase.io import read, write
+from ase.optimize import BFGS
+from xtb.ase.calculator import XTB
+
+import xyz2mol as x2m
+
+
+def get_options(argv):
+   '''
+   Read in cmd line arguments
+   ligand: ligand geometry (sdf)
+   target: target geometry (sdf)
+   forceField: force field to be used
+   trajectories: how many trajectorues (MC runs) per conformer
+   steps: number of MC steps
+   temperature: temperature for acceptance test
+   '''
+   #opts, args = getopt.getopt(argv, "hl:t:f:j:s:u:m:", ["help", "ligand=", "target=", "forceField=", "trajectories=", "steps=", "temperature=", "mutations="])
+   opts, args = getopt.getopt(argv, "hl:t:f:j:s:u:", ["help", "ligand=", "target=", "forceField=", "trajectories=", "steps=", "temperature="])
+   target, ligand, ff, trajectories, steps, temperature = "PA_hexamer.sdf", "Ru_guest_for_PA.sdf", "UFF", 5, 200, 0.3
+   for opt, arg in opts:
+      if opt == '--help':
+         print("How to run:")
+         print('test.py --ligand <ligand> --target <target> --forceField <force field> --trajectories # --steps # --temperature\n')
+         print(" -> ligand and target molecules as .sdf files")
+         print(" -> force fileds as implemented in openbabel (UFF, MMFF94, ...)")
+         print(" -> trajectories:\t# of trajectories")
+         print(" -> steps:\tnumber of MC steps")
+         print(" -> temperature for acceptance step\n")
+         print(" -> mutations: # of mutations\n")
+         sys.exit()
+      elif opt in ("-l", "--ligand"):
+         ligand = arg
+      elif opt in ("-t", "--target"):
+         target = arg
+      elif opt in ("-f", "--forceField"):
+         ff = arg
+      elif opt in ("-j", "--trajectories"):
+         trajectories = arg
+      elif opt in ("-s", "--steps"):
+         steps = arg
+      elif opt in ("-u", "--temperature"):
+         temperature = arg
+#      elif opt in ("-m", "--mutations"):
+#          mutations = arg
+
+   #return target, ligand, ff, trajectories, steps, temperature, mutations
+   return target, ligand, ff, trajectories, steps, temperature
+
+
+def readfile(f):
+    '''
+    input:   filename (sdf)
+    returns: rdkit mol object
+    '''
+    mol = Chem.MolFromMolFile(f, removeHs=False, sanitize=True)
+    mol = Chem.RWMol(mol)
+    return mol
+
+
+def writefile(mol, f):
+    '''
+    input:   mol object, filename (sdf)
+    returns: -
+    '''
+    print(Chem.MolToMolBlock(mol), file=open(f, 'w+'))
+
+def writefile_xyz(mol, f):
+    numAtoms = mol.GetNumAtoms()
+    labels = []
+    coords = []
+
+    NAME = {1:"H", 6:"C", 7:"N", 8:"O", 44:"Ru"}
+    for i, atom in enumerate(mol.GetAtoms()):
+        label = atom.GetAtomicNum()
+        labels.append(label)
+        positions = mol.GetConformer().GetAtomPosition(i)
+        coords.append([positions.x, positions.y, positions.z])
+
+    labels = [NAME[label] for label in labels]
+
+    fout = open(f, 'w')
+    fout.write("{}\n\n".format(numAtoms))
+
+    for i, label in enumerate(labels):
+        fout.write("{} {} {} {}\n".format(label, coords[i][0], coords[i][1], coords[i][2]))
+
+
+    fout.close()
+
+def get_com(mol):
+    '''
+    get center of mass
+    '''
+    com = np.array([0.0, 0.0, 0.0])
+
+    for i in range(mol.GetNumAtoms()):
+        positions = mol.GetConformer().GetAtomPosition(i)
+        tmp = np.array([ positions.x, positions.y, positions.z ])
+        com += tmp
+
+    com /= mol.GetNumAtoms()
+
+    return com
+
+def move_molecule(mol, move, startid=0, endid=-1):
+    '''
+    move molecule using move vector
+    '''
+
+    if endid == -1:
+        endid = mol.GetNumAtoms()
+
+    for i in range(startid, endid):
+        positions = mol.GetConformer().GetAtomPosition(i)
+        temp = np.array([positions.x, positions.y, positions.z])
+        temp += move
+        mol.GetConformer().SetAtomPosition(i, Point3D(temp[0], temp[1], temp[2]))
+
+    return mol
+
+def minimize_molecule(mol, force_field):
+    '''
+    optimizes molecule using a force field
+    returns optimized geometry and energy
+    '''
+    res = AllChem.UFFOptimizeMoleculeConfs(mol)
+    e = res[0][1]
+
+    return e, mol
+
+def minimize_molecule_xTB(mol):
+    cmd = "ulimit -s unlimited"
+    os.system(cmd)
+    writefile_xyz(mol, "run_opt.xyz")
+    mol_ase = read('run_opt.xyz')
+
+    calc = XTB(method="GFN2-xTB")
+    mol_ase.calc = calc
+
+    dyn = BFGS(mol_ase)
+    dyn.run(fmax=0.50)
+
+    e = mol_ase.get_potential_energy()
+
+    write("run_min.xyz", mol_ase)
+
+    cmd = "python ~/workcopies/xyz2mol/xyz2mol.py run_min.xyz -o sdf > run_min.sdf"
+    os.system(cmd)
+
+    mol_min = readfile("run_min.sdf")
+
+    cmd = "rm -f run_opt.xyz run_min.xyz run_min.sdf"
+    os.system(cmd)
+
+    return e, mol_min
+
+def calc_energy_xTB(mol):
+    cmd = "ulimit -s unlimited"
+    os.system(cmd)
+    writefile_xyz(mol, "run_sp.xyz")
+    mol_ase = read('run_sp.xyz')
+
+    calc = XTB(method="GFN2-xTB")
+    mol_ase.calc = calc
+
+    e = mol_ase.get_potential_energy()
+
+    cmd = "rm -f xtbrestart xtbtopo.mol run_sp.xyz wbo charges"
+    os.system(cmd)
+
+    return e
+
+
+
+def rotate(V, J, T):
+    x = V[0]
+    y = V[1]
+    z = V[2]
+
+    u = J[0]
+    v = J[1]
+    w = J[2]
+
+    norm = np.sqrt(u*u + v*v + w*w)
+    inv_norm_sqrt = 1.0 / (norm*norm)
+
+    sint = np.sin(T)
+    cost = np.cos(T)
+
+    a = (u * (u*x + v*y + w*z) + (x * (v*v + w*w) - u * (v*y + w*z)) * cost + norm * (-w*y + v*z) * sint) * inv_norm_sqrt
+    b = (v * (u*x + v*y + w*z) + (y * (u*u + w*w) - v * (u*x + w*z)) * cost + norm * ( w*x - u*z) * sint) * inv_norm_sqrt
+    c = (w * (u*x + v*y + w*z) + (z * (u*u + v*v) - w * (u*x + v*y)) * cost + norm * (-v*x + u*y) * sint) * inv_norm_sqrt
+
+    rotated = np.array([a, b, c])
+
+    return rotated
+
+def rotate_molecule(mol, direction, theta, startid=0, endid=-1):
+    '''
+    rotate molecule using rot matrix, theta
+    '''
+    if endid == -1:
+        endid = mol.GetNumAtoms()
+
+    com = np.array([0.0, 0.0, 0.0])
+
+    for i in range(startid, endid):
+        positions = mol.GetConformer().GetAtomPosition(i)
+        com += np.array([positions.x, positions.y, positions.z])
+
+    com /= (endid - startid)
+
+    for i in range(startid, endid):
+        positions = mol.GetConformer().GetAtomPosition(i)
+        temp = np.array([positions.x, positions.y, positions.z])
+        temp -= com
+        mol.GetConformer().SetAtomPosition(i, Point3D(temp[0], temp[1], temp[2]))
+        temp = rotate(temp, direction, theta)
+        temp += com
+        mol.GetConformer().SetAtomPosition(i, Point3D(temp[0], temp[1], temp[2]))
+
+def random_vector():
+    '''
+    generates random 3D vector [-1, 1]
+    '''
+    rng = np.random.default_rng()
+    vec = rng.random((3,))
+    x = np.random.choice([-1., 1.], size=1)
+    y = np.random.choice([-1., 1.], size=1)
+    z = np.random.choice([-1., 1.], size=1)
+
+    vec_tmp = np.array([ x*vec[0], y*vec[1], z*vec[2] ])
+    vec = vec_tmp.reshape(3,)
+
+    return vec
+
+def uniform1():
+    '''
+    returns random number [0, 1] (scalar)
+    '''
+    rng = np.random.default_rng()
+    return rng.random()
+
+def random_angle():
+    '''
+    returns random angle for molecule rotation (scalar)
+    '''
+    rng = np.random.default_rng()
+    return 90./180.*np.pi * rng.random()
+
+def random_length():
+    '''
+    returns random length [0, 0.3] (scalar)
+    '''
+    rng = np.random.default_rng()
+    #return 2 * rng.random()
+    ranInt = rng.integers(low=0, high=30, size=1)
+
+
+    if ranInt == 0:
+        return 0
+    else:
+        return ranInt / 100.
+
+def set_conformations(mol, force_field):
+    '''
+    Conformer search
+    '''
+    ff = ob.OBForceField.FindForceField(force_field)
+    ff.Setup(mol)
+
+    rmsd_cutoff   = 0.5
+    energy_cutoff = 50.0
+    conf_cutoff   = 10000000
+    verbose       = False
+
+    ff.DiverseConfGen(rmsd_cutoff, conf_cutoff, energy_cutoff, verbose)
+    ff.GetConformers(mol)
+
+
+    return mol
+
+def plus_equal_mols(target, ligand, numTot):
+    '''
+    mol += mol2 does not exist in the python wrapper from openbabel.
+    Therefore, this function adds the ligand to the target molecule (same mol object).
+    In the first step (# of atoms of target < # num atoms target+ligand) it is simply added
+    by creating the ligand atoms and bonds.
+    For every subsequent step, the ligand atom positions are simply updated
+    '''
+    #ligand = ob.OBMol()
+    #ligand = readfile_xyz("Ru_guest_for_PA.xyz")
+
+    if target.GetNumAtoms() < numTot:
+        mol_new = target
+        numAtoms = target.GetNumAtoms()
+
+        atom0      = []
+        atom1      = []
+        bond_order = []
+
+        for bond in ligand.GetBonds():
+            atom0.append(bond.GetBeginAtomIdx()+numAtoms)
+            atom1.append(bond.GetEndAtomIdx()+numAtoms)
+            bond_order.append(bond.GetBondType())
+
+        for i, atom in enumerate(ligand.GetAtoms()):
+            atomic_num = atom.GetAtomicNum()
+            positions = ligand.GetConformer().GetAtomPosition(i)
+            temp = np.array([positions.x, positions.y, positions.z])
+
+            mol_new.AddAtom(Chem.Atom(atomic_num))
+            mol_new.GetConformer().SetAtomPosition(i+numAtoms, Point3D(temp[0], temp[1], temp[2]))
+
+        for i in range(len(atom0)):
+            mol_new.AddBond(atom0[i], atom1[i], bond_order[i])
+
+    else:
+        mol_new = target
+        numAtoms = target.GetNumAtoms() - ligand.GetNumAtoms()
+
+        for i in range(ligand.GetNumAtoms()):
+            positions = ligand.GetConformer().GetAtomPosition(i)
+            X = positions.x
+            Y = positions.y
+            Z = positions.z
+
+            mol_new.GetConformer().SetAtomPosition(numAtoms+i, Point3D(X, Y, Z))
+
+    Chem.SanitizeMol(mol_new)
+
+    return mol_new
+
